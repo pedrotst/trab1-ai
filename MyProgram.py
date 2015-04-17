d9465e299c791fd8b809838548c16d211dc8cbf9
EricFS <eric.robotic@gmail.com>
2015-04-17 17:07:17 +1000

    stratified

--- MyProgram.py
+++ MyProgram.py
@@ -1,6 +1,6 @@
 import statistics
 import math
-#math.exp(x)
+
 class bayesstats():
 	mean_yes = None
 	mean_no = None
@@ -8,7 +8,7 @@ class bayesstats():
 	dev_no = None
 	def __init__(self, mean_yes, mean_no, dev_yes, dev_no):
 		self.mean_yes = mean_yes
-		self.mean_n = mean_no
+		self.mean_no = mean_no
 		self.dev_yes = dev_yes
 		self.dev_no = dev_no
 
@@ -21,6 +21,16 @@ class bayesstats():
 
 	def __repr__(self):
 		return self.__str__()
+
+def folding(training_set, fold_num):
+	training_set_folded = []
+	test_folded = []
+	for i in range(len(training_set)):
+		if i % 10 == fold_num:
+			test_folded.append(training_set[i])
+		else:
+			training_set_folded.append(training_set[i])
+	return test_folded, training_set_folded
 
 def mean_dev(training_set):
 	class_yes = []
@@ -46,19 +56,41 @@ def mean_dev(training_set):
 		class_yes = []
 		class_no = []
 	return mean_yes, mean_no, dev_yes, dev_no, prob_yes, prob_no
-	# print(mean_yes, dev_yes, prob_yes, mean_no, dev_no, prob_no)
 
 def naive_bayes_training(training_set):
     trained_values = {}
     mean_yes, mean_no, dev_yes, dev_no, prob_yes, prob_no = mean_dev(training_set)
     for key in training_set[0]:
     	if not key == 'DiabetesClass':
-    		trained_values[key] = bayesstats(mean_yes[key], mean_no[key], dev_yes[key], dev_no[key])
+    		trained_values[key] = bayesstats(float(mean_yes[key]), float(mean_no[key]), float(dev_yes[key]), float(dev_no[key]))
     	else:
     		trained_values[key] = {'prob_yes': prob_yes, 'prob_no': prob_no}
     return trained_values
 
-def naive_bayes_testing(test):
+def prob_density(new_value, mean, dev):
+	chunk_1 = (new_value - mean) ** 2
+	chunk_2 = 2 * ((dev) ** 2)
+	chunk_3 = - (chunk_1 / chunk_2)
+	chunk_4 = math.exp(chunk_3)
+	chunk_5 = dev * ((2 * math.pi) ** 0.5)
+	chunk_6 = 1 / chunk_5
+	chunk_7 = chunk_6 * chunk_4
+	return chunk_7
+
+def naive_bayes_testing(test, trained_values):
+	prob_yes = 1
+	prob_no = 1
+	for key in trained_values:
+		if not key == 'DiabetesClass':
+			prob_yes *= prob_density(test[key], trained_values[key].mean_yes, trained_values[key].dev_yes)
+			prob_no *= prob_density(test[key], trained_values[key].mean_no, trained_values[key].dev_no)
+		else:
+			prob_yes *= trained_values[key]['prob_yes']
+			prob_no *= trained_values[key]['prob_no']
+	if prob_yes >= prob_no:
+		return 'yes'
+	else:
+		return 'no'
 
 
 f_training = open('pima.csv','r')#later use user given address
@@ -76,7 +108,22 @@ for line in f_training:
     training_set[i]['PedigreeFunction'] = float(line[6])
     training_set[i]['Age'] = float(line[7])
     training_set[i]['DiabetesClass'] = line[8].rstrip('\n')
-    # print(training_set[i])
     i+=1
-# TrainedValues = naive_bayes_training(training_set)
-trined_values = naive_bayes_training(training_set)
+
+training_set = sorted(training_set, key = lambda k: k['DiabetesClass'])
+test_folded, training_set_folded = folding(training_set, 0)
+trained_values = naive_bayes_training(training_set_folded)
+
+# raw_test = '0.058824,0.316129,0.469388,0.26087,0.169471,0.249489,0.101196,0.033333'
+# raw_test = raw_test.split(',')
+# test = {}
+# test['TimesPregnant'] = float(raw_test[0])
+# test['GlucoseConcentration'] = float(raw_test[1])
+# test['DiastolicPressure'] = float(raw_test[2])
+# test['SkinThickness'] = float(raw_test[3])
+# test['SerumInsulin'] = float(raw_test[4])
+# test['MassIndex'] = float(raw_test[5])
+# test['PedigreeFunction'] = float(raw_test[6])
+# test['Age'] = float(raw_test[7])
+for i in range(len(test_folded)):
+	print(naive_bayes_testing(test_folded[i], trained_values))
